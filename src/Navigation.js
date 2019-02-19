6b6c87c153e46721d2869e2194122bf11a49ae48
Justin Lautz <lautzjd@gmail.com>
2019-02-18 21:18:05 -0600

    NavChangedToJavaScript

---
+++ src/Navigation.js
@@ -0,0 +1,27 @@
+export default `
+<div id="navigation">
+<ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="blog/">Blog</a></li>
+    <li><a href="Contact/">Contact</a></li>
+    <li><a href="Projects/">Projects</a>
+
+        <ul>
+            <li><a href="">First</a></li>
+            <li><a href="">Second</a></li>
+            <li><a href="">Third</a></li>
+
+        </ul>
+    </li>
+
+</ul>
+
+<div>
+    <img src="https://i.pinimg.com/originals/e0/c1/7f/e0c17f52e48a1dc98a989c6701b526c5.jpg"
+        alt="SCCA Logo">
+
+</div>
+
+
+</div>
+`;
