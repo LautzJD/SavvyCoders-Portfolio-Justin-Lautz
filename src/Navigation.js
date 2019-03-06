bcf56a72806c92e413c99225c582462262e9077d
Justin Lautz <lautzjd@gmail.com>
2019-03-05 19:58:22 -0600

    Refactor link builder to use forEach loop instead of while

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,33 @@
 import { lowerCase } from 'lodash';
 
 function linkBuilder(linksArray){
-    var i = 0;
+    // var i = 0;
     var linksHTML = '';
+    var destination = '';
 
-    while(i < linksArray.length){
+    // while(i < linksArray.length){
+    //     if(linksArray[i] !== 'Home'){
+    //         destination = lowerCase(linksArray[i]);
+    //     }
+    //     linksHTML += `
+    //     <li>
+    //     <a data-navigo href="./${destination}">${linksArray[i]}</a>
+    //     </li>
+    //     `;
+        
+    //     i++;
+    // }
+
+    linksArray.forEach((link) =>  { // 'link' represents each individual 'link' in the link aray
+        if(link !== 'Home'){
+            destination = lowerCase(link);
+        }
         linksHTML += `        
         <li>
-        <a data-navigo href="./${lowerCase(linksArray[i])}">${linksArray[i]}</a>
+        <a data-navigo href="./${destination}">${link}</a>
         </li>
         `;
-        
-        i++;
-    }
+    });
     
     return linksHTML;
 }
