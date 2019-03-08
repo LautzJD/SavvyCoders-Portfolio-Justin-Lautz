60938d5337880508c8e6f84038f78c161dc9cc79
Justin Lautz <lautzjd@gmail.com>
2019-03-07 21:10:00 -0600

    Refactor Navigation to use map()

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,48 +1,65 @@
+
 import { lowerCase } from 'lodash';
 
 function linkBuilder(linksArray){
-    // var i = 0;
-    var linksHTML = '';
-    var destination = '';
-
-    // while(i < linksArray.length){
-    //     if(linksArray[i] !== 'Home'){
-    //         destination = lowerCase(linksArray[i]);
-    //     }
-    //     linksHTML += `
-    //     <li>
-    //     <a data-navigo href="./${destination}">${linksArray[i]}</a>
-    //     </li>
-    //     `;
+    var linksHTML = linksArray.map((link) => {
+        var destination = '';
         
-    //     i++;
-    // }
-
-    linksArray.forEach((link) =>  { // 'link' represents each individual 'link' in the link aray
         if(link !== 'Home'){
             destination = lowerCase(link);
         }
-        linksHTML += `        
+        
+        return `
         <li>
         <a data-navigo href="./${destination}">${link}</a>
         </li>
         `;
-    });
+    }).join(' ');
+    
+    console.log(linksHTML);
     
     return linksHTML;
 }
 
 export default function Navigation(state){
     return `
-<div id="navigation">
+     < div id = "navigation" >
 
-<ul>
-${linkBuilder(state.links)}
-</ul>
+                <ul>
+                    ${linkBuilder(state.links)}
+                </ul>
 
-<img src="https://i.pinimg.com/originals/e0/c1/7f/e0c17f52e48a1dc98a989c6701b526c5.jpg"
-        alt="DNA Graphic">
+                <img src="https://i.pinimg.com/originals/e0/c1/7f/e0c17f52e48a1dc98a989c6701b526c5.jpg"
+                    alt="DNA Graphic">
 
-</div>
-`;
+            </div>
+    `;
 }
+
+// var i = 0;
+// while(i < linksArray.length){
+//     if(linksArray[i] !== 'Home'){
+//         destination = lowerCase(linksArray[i]);
+//     }
+//     linksHTML += `
+//     <li>
+//     <a data-navigo href="./${destination}">${linksArray[i]}</a>
+//     </li>
+//     `;
+        
+//     i++;
+// }
+        
+
+// linksArray.forEach((link) =>  { // 'link' represents each individual 'link' in the link aray
+//     if(link !== 'Home'){
+//         destination = lowerCase(link);
+//     }
+//     linksHTML += `
+//         < li >
+//         <a data-navigo href="./${destination}">${link}</a>
+//     </li >
+//         `;
+// });
+
+
